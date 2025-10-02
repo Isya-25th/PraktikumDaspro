ce0d6a2154124081bca78006b88c9d03ea273073
Isya-25th <isyaputra.r25th@gmail.com>
2025-10-02 09:30:20 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS13.java
+++ jobsheet5/ifCetakKRS13.java
@@ -15,6 +15,9 @@ public class ifCetakKRS13 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         }
+        else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
 
         sc.close();
     }
