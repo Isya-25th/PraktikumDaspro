cd1d2949ae7338b7fcfa23255beaf8236420b922
Isya-25th <isyaputra.r25th@gmail.com>
2025-10-02 10:05:29 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS13.java
+++ jobsheet5/ifCetakKRS13.java
@@ -11,13 +11,11 @@ public class ifCetakKRS13 {
         System.out.print("Apakah sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas){
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        }
-        else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        String pesan = "";
+
+        pesan = (uktLunas == true) ? "Pembayaran UKT terverifikasi. Silakan cetak KRS" : "Regristasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
 
         sc.close();
     }
